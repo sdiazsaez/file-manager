0b0961c371fe1bc568d327746b8c072c264443c6
simon <sdiaz.sz@gmail.com>
2019-07-19 18:41:01 -0400

    - add getExtension - add MimeType

--- src/Http/Controllers/FileManager/FileManagerController.php
+++ src/Http/Controllers/FileManager/FileManagerController.php
@@ -3,7 +3,6 @@
 namespace Larangular\FileManager\Http\Controllers\FileManager;
 
 use Illuminate\Http\UploadedFile;
-use Larangular\FileManager\Models\FileManager;
 
 class FileManagerController {
 
@@ -11,25 +10,39 @@ class FileManagerController {
     }
 
     public function uploadFile(UploadedFile $file, ?string $disk = null, ?string $path = null) {
-        if(is_null($disk)) {
+        if (is_null($disk)) {
             $disk = config('file-manager.disk_name');
         }
 
-        if(is_null($path)) {
+        if (is_null($path)) {
             $path = '/';
         }
 
         $storedFile = $file->store($path, $disk);
 
         return [
-            'disk' => $disk,
-            'path' => $path,
-            'name' => $file->hashName(),
+            'disk'          => $disk,
+            'path'          => $path,
+            'name'          => $file->hashName(),
             'original_name' => $file->getClientOriginalName(),
-            'extension' => $file->extension(),
-            'type' => $file->getMimeType(),
-            'size' => $file->getSize(),
+            'extension'     => $this->getExtension($file),
+            'type'          => $this->getMimeType($file),
+            'size'          => $file->getSize(),
         ];
+    }
+
+    private function getExtension(UploadedFile $file): string {
+        $ext = $file->extension();
+        return is_null($ext)
+            ? $file->getClientOriginalExtension()
+            : $ext;
+    }
+
+    private function getMimeType(UploadedFile $file): string {
+        $type = $file->getMimeType();
+        return is_null($type)
+            ? $file->getClientMimeType()
+            : $type;
     }
 
 }
