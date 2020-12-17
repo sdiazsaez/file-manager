64932c098faf6c40819ac271f5bfaead24d62126
simon <sdiaz.sz@gmail.com>
2020-12-17 10:44:29 -0300

    [Update] remove resources and update routing-controller

--- src/Http/Controllers/FileManager/Gateway.php
+++ src/Http/Controllers/FileManager/Gateway.php
@@ -2,28 +2,18 @@
 
 namespace Larangular\FileManager\Http\Controllers\FileManager;
 
-use \Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
-use Larangular\FileManager\Models\FileManager;
-use Larangular\FileManager\Http\Resources\FileManagerResource;
 use Larangular\FileManager\Facades\FileManagerController;
-use Larangular\RoutingController\{Controller,
-    Contracts\HasPagination,
-    Contracts\HasResource,
-    Contracts\IGatewayModel,
-    Contracts\RecursiveStoreable,
-    RecursiveStore\RecursiveOption};
+use Larangular\FileManager\Http\Resources\FileManagerResource;
+use Larangular\FileManager\Models\FileManager;
+use Larangular\RoutingController\{Contracts\IGatewayModel, Controller};
 
 
-class Gateway extends Controller implements IGatewayModel, HasResource {
+class Gateway extends Controller implements IGatewayModel {
 
     public function model() {
         return FileManager::class;
-    }
-
-    public function resource() {
-        return FileManagerResource::class;
     }
 
     public function save($data) {
@@ -46,7 +36,8 @@ class Gateway extends Controller implements IGatewayModel, HasResource {
 
     public function showFile(FileManager $fileManager) {
         $filePath = $fileManager->path . DIRECTORY_SEPARATOR . $fileManager->name;
-        $storage = Storage::disk($fileManager->disk)->path($filePath);
+        $storage = Storage::disk($fileManager->disk)
+                          ->path($filePath);
         return response()->file($storage);
         //return Image::make()->response('png');
     }
